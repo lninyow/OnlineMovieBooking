82999b32dbb72e193dc034be00704b7386d35e23
Liden <lninyow@gmail.com>
2023-04-27 08:50:01 +0800

    Changed resolution of frame

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame newFrame = new JFrame("Test");
-        newFrame.setSize(500,500);
+        newFrame.setSize(1920,1080);
         newFrame.setVisible(true);
     }
 }
