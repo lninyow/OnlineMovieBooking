1193bb677a9c915035b665c7a7fa6673d4c65f3f
Liden <lninyow@gmail.com>
2023-04-27 08:47:40 +0800

    Added Frame

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+        JFrame newFrame = new JFrame("Test");
+        newFrame.setSize(500,500);
+        newFrame.setVisible(true);
+    }
+}
