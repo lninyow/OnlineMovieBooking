d8a62116af7551abe892301c2a7099a063fdcaf4
User <User@LAPTOP-8P9UA4KC>
2023-04-28 12:42:15 +0800

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
-import javax.swing.*;
-public class Main {
+    import javax.swing.*;
+    import java.awt.event.*;
+    
+    public class Main {
     public static void main(String[] args) {
         JFrame newFrame = new JFrame("Test");
         newFrame.setSize(1920,1080);
+
+        JLabel label = new JLabel("Hello World!");
+        label.setBounds(50, 50, 100, 30);
+        newFrame.add(label);
+
+        JButton button = new JButton("Click Me!");
+        button.setBounds(50, 100, 100, 30);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                label.setText("Button Clicked!");
+            }
+        });
+        newFrame.add(button);
+
+        newFrame.setLayout(null);
         newFrame.setVisible(true);
     }
 }
