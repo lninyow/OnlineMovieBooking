e83a1beb571688113cfd387780c65e0e582389b7
LAB14_USER ON WS16 <L14Y08W16@citulab.net>
2023-05-02 10:03:45 +0800

    Edited Welcome Page part 2 unfinished

--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -14,38 +14,67 @@ public class WelcomePage extends JFrame {
     public WelcomePage() {
         super("Login Page");
             // Create the login panel
-            loginPanel = new JPanel();
-            loginPanel.setPreferredSize(new Dimension(800, 900));
-            loginPanel.setLayout(null);
+        // Create the login panel
+        loginPanel = new JPanel();
+        loginPanel.setPreferredSize(new Dimension(800, 900));
+        loginPanel.setLayout(new GridBagLayout());
 
-            // Create the welcome label
-            welcomeLabel = new JLabel("WELCOME");
-            welcomeLabel.setFont(new Font("SansSerif", Font.BOLD, 40));
-            welcomeLabel.setBounds(300, 50, 200, 50);
-            loginPanel.add(welcomeLabel);
+        // Create the welcome label
+        welcomeLabel = new JLabel("WELCOME");
+        welcomeLabel.setFont(new Font("SansSerif", Font.BOLD, 40));
+        GridBagConstraints welcomeConstraints = new GridBagConstraints();
+        welcomeConstraints.gridx = 0;
+        welcomeConstraints.gridy = 0;
+        welcomeConstraints.insets = new Insets(0, 100, 50, 0);
+        loginPanel.add(welcomeLabel, welcomeConstraints);
 
-            // Create the username label and field
-            usernameLabel = new JLabel("Username:");
-            usernameLabel.setBounds(150, 150, 100, 25);
-            loginPanel.add(usernameLabel);
+        // Create the username label and field
+        usernameLabel = new JLabel("Username:");
+        usernameLabel.setFont(new Font("",Font.PLAIN,15));
+        GridBagConstraints usernameLabelConstraints = new GridBagConstraints();
+        usernameLabelConstraints.gridx = 0;
+        usernameLabelConstraints.gridy = 1;
+        usernameLabelConstraints.insets = new Insets(0, 0, 10, 0);
+        loginPanel.add(usernameLabel, usernameLabelConstraints);
 
-            usernameField = new JTextField();
-            usernameField.setBounds(250, 150, 300, 25);
-            loginPanel.add(usernameField);
+        usernameField = new JTextField(20);
+        usernameField.setPreferredSize(new Dimension(200, 30));
+        GridBagConstraints usernameFieldConstraints = new GridBagConstraints();
+        usernameFieldConstraints.gridx = 1;
+        usernameFieldConstraints.gridy = 1;
+        usernameFieldConstraints.fill = GridBagConstraints.HORIZONTAL;
+        usernameFieldConstraints.insets = new Insets(10, 0, 10, 0);
+        loginPanel.add(usernameField, usernameFieldConstraints);
 
-            // Create the password label and field
-            passwordLabel = new JLabel("Password:");
-            passwordLabel.setBounds(150, 200, 100, 25);
-            loginPanel.add(passwordLabel);
+        // Create the password label and field
+        passwordLabel = new JLabel("Password:");
+        passwordLabel.setFont(new Font("",Font.PLAIN,15));
+        GridBagConstraints passwordLabelConstraints = new GridBagConstraints();
+        passwordLabelConstraints.gridx = 0;
+        passwordLabelConstraints.gridy = 2;
+        passwordLabelConstraints.insets = new Insets(0, 0, 10, 0);
+        loginPanel.add(passwordLabel, passwordLabelConstraints);
 
-            passwordField = new JPasswordField();
-            passwordField.setBounds(250, 200, 300, 25);
-            loginPanel.add(passwordField);
+        passwordField = new JPasswordField();
+        passwordField.setPreferredSize(new Dimension(200, 30));
+        GridBagConstraints passwordFieldConstraints = new GridBagConstraints();
+        passwordFieldConstraints.gridx = 1;
+        passwordFieldConstraints.gridy = 2;
+        passwordFieldConstraints.fill = GridBagConstraints.HORIZONTAL;
+        passwordFieldConstraints.insets = new Insets(0, 0, 10, 0);
+        loginPanel.add(passwordField, passwordFieldConstraints);
 
-            // Create the login button
-            loginButton = new JButton("Log in");
-            loginButton.setBounds(250, 250, 100, 25);
-            loginPanel.add(loginButton);
+        // Create the login button
+        loginButton = new JButton("Log in");
+        Color buttonColor = new Color(65, 75, 178);
+        loginButton.setPreferredSize(new Dimension(215,35));
+        loginButton.setBackground(buttonColor);
+        loginButton.setForeground(Color.WHITE);
+        GridBagConstraints loginButtonConstraints = new GridBagConstraints();
+        loginButtonConstraints.gridx = 1;
+        loginButtonConstraints.gridy = 3;
+        loginButtonConstraints.anchor = GridBagConstraints.LINE_END;
+        loginPanel.add(loginButton, loginButtonConstraints);
 
             // Create the image label
             ImageIcon imageIcon = new ImageIcon("image.jpg");
