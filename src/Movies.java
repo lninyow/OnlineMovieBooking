22d2fc88fa231a1305987018519d400f1e042500
Hana <hanako0311>
2023-05-12 00:08:55 +0800

    Booking Page

--- src/Movies.java
+++ src/Movies.java
@@ -3,7 +3,7 @@ public class Movies {
    private int  id;
    private String title;
    private int release_year;
-    private String director;
+   private String director;
    private String genre;
-    private String plot_summary;
+   private String plot_summary;
 }
