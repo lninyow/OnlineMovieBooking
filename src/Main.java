d007f35bc6c086e6781e65eaad9b9322e955944d
Liden <lninyow@gmail.com>
2023-05-20 21:19:07 +0800

    Connected welcome Page, home page, register page

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,6 @@
     public static void main(String[] args) throws SQLException {
 
       new WelcomePage();
-       //HomePageTest newHomepage = new HomePageTest();
-       //    newHomepage.setVisible(true);
-        //RegisterPage registrationPage = new RegisterPage();
-        //registrationPage.setVisible(true);
-
-
-
 
     }
 }
